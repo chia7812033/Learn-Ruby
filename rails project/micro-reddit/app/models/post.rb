2dbb631769b2b900c36ab72e0ffea0671272baf4
chia7812033 <dan891221@gmail.com>
2022-12-26 12:00:53 +0800

    Move to elsewhere

--- rails project/micro-reddit/app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user
-  has_many :comments
-
-  validates :title, presence: true, length: { minimum: 6 }
-  validates :body, presence: true, length: { minimum: 20 }
-end
